fa7290faf009851896e6c3fdf8786f3273bcc7bd
LeandroDornela <leandrodornela@ice.ufjf.br>
2021-03-22 14:54:23 -0300

    Adiciona controle de play/pause

--- js/Main.js
+++ js/Main.js
@@ -6,10 +6,22 @@ const cena1 = new Cena(canvas);
 
 cena1.desenhar();
 
-const pc = new Sprite({x:0,y:0,w:32,h:32,color:"blue"});
+const pc = new Sprite({x:0,y:0,w:32,h:32,color:"blue",vx:10});
 const en1 = new Sprite({x:100,y:100,w:32,h:32,color:"red"});
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
 
-cena1.quadro(0);
+cena1.iniciar();
+
+document.addEventListener("keydown", (e)=>{
+    switch(e.key)
+    {
+        case "s":
+            cena1.iniciar();
+            break;
+        case "d":
+            cena1.parar();
+            break;
+    }
+})
