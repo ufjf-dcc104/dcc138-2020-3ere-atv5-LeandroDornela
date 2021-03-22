6cc6bea44ed92cfeb7d35c94f9f3109c1840d3d9
LeandroDornela <leandrodornela@ice.ufjf.br>
2021-03-22 14:40:20 -0300

    Adiciona passo na cena e sprite

--- js/Main.js
+++ js/Main.js
@@ -2,12 +2,12 @@ import Cena from "./Cena.js";
 import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 const cena1 = new Cena(canvas);
 
 cena1.desenhar();
 
 const pc = new Sprite({x:0,y:0,w:32,h:32,color:"blue"});
 const en1 = new Sprite({x:100,y:100,w:32,h:32,color:"red"});
-pc.desenhar(ctx);
-en1.desenhar(ctx);
+
+cena1.adicionar(pc);
+cena1.adicionar(en1);
--- js/Sprite.js
+++ js/Sprite.js
@@ -1,17 +1,26 @@
 export default class Sprite
 {
-    constructor({x=0,y=0,w=0,h=0,color="black"})
+    constructor({x=0,y=0,w=0,h=0,color="black",vx=0,vy=0})
     {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.color = color;
+
+        this.vx = vx;
+        this.vy = vy;
     }
 
     desenhar(ctx)
     {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    passo(dt)
+    {
+        this.x = this.x + this.vx * dt;
+        this.y = this.y + this.vy * dt;
     }
 }
