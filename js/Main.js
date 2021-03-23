beaf0df40c5554cf9a6b2aa6410bfb90407dc22b
LeandroDornela <leandrodornela@ice.ufjf.br>
2021-03-23 09:36:15 -0300

    correção bug mapa

--- js/Main.js
+++ js/Main.js
@@ -3,19 +3,23 @@ import Cena from "./Cena.js";
 import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
+import {map1 as modeloMapa1} from "../maps/map1.js";
 
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 const canvas = document.querySelector("canvas");
 canvas.width = 16*32;
 canvas.height = 9*32;
+const mapa1 = new Mapa(9, 16, 32);
 const cena1 = new Cena(canvas, assets);
-const mapa1 = new Mapa();
+
 
 const pc = new Sprite({x:0,y:100,w:32,h:32,color:"blue",vx:10});
 const en1 = new Sprite({x:100,y:100,w:32,h:32,color:"red"});
 
+mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
+
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("coin", "assets/coin.wav")
 
