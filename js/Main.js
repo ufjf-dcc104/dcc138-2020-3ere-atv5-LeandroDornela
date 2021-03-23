cb50dcb1dd9663316f5541856c228026928ff0a3
LeandroDornela <leandrodornela@ice.ufjf.br>
2021-03-23 09:05:30 -0300

    cria uma mapa para uso na cena

--- js/Main.js
+++ js/Main.js
@@ -1,25 +1,31 @@
 import AssetManager from "./AssetManager.js";
 import Cena from "./Cena.js";
+import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
-assets.carregaImagem("orc", "assets/orc.png");
-assets.carregaAudio("coin", "assets/coin.wav")
-
 const canvas = document.querySelector("canvas");
+canvas.width = 16*32;
+canvas.height = 9*32;
 const cena1 = new Cena(canvas, assets);
-
-cena1.desenhar();
+const mapa1 = new Mapa();
 
 const pc = new Sprite({x:0,y:100,w:32,h:32,color:"blue",vx:10});
 const en1 = new Sprite({x:100,y:100,w:32,h:32,color:"red"});
+
+cena1.configuraMapa(mapa1);
+assets.carregaImagem("orc", "assets/orc.png");
+assets.carregaAudio("coin", "assets/coin.wav")
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
 
 cena1.iniciar();
+
+cena1.desenhar();
+
 
 document.addEventListener("keydown", (e)=>{
     switch(e.key)
---
+++ js/Mapa.js
@@ -0,0 +1,44 @@
+export default class Mapa
+{
+    constructor(lines=8, columns=12, size=32)
+    {
+        this.LINES = lines;
+        this.COLUMNS = columns;
+        this.SIZE = size;
+        this.tiles = [];
+
+        for (let l = 0; l < this.LINES; l++) {
+            this.tiles[l] = [];
+            for (let c = 0; c < this.COLUMNS; c++) {
+                this.tiles[l][c] = 0;      
+            }
+        }
+
+        this.cena = null;
+    }
+
+    desenhar(ctx)
+    {
+        for (let l = 0; l < this.LINES; l++) {
+            this.tiles[l] = [];
+            for (let c = 0; c < this.COLUMNS; c++) {
+                switch(this.tiles[l][c])
+                {
+                    case 1:
+                        ctx.fillStyle = "grey";
+                        ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        ctx.lineWidth = 1;
+                        ctx.strokeStyle = "black";
+                        ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        break;
+                        default:
+                        ctx.fillStyle = "black";
+                        ctx.fillRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                        ctx.lineWidth = 1;
+                        ctx.strokeStyle = "grey";
+                        ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE);
+                }      
+            }
+        }
+    }
+}
