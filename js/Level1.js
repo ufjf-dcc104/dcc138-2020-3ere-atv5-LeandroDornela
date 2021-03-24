035dbc198e710a346465cbef94c1a8fd83752673
LeandroDornela <leandrodornela@ice.ufjf.br>
2021-03-24 15:46:31 -0300

    atv6 - level1

--- js/Level1.js
+++ js/Level1.js
@@ -103,25 +103,4 @@ export default class Level1 extends Scene
     {
         return (Math.floor(Math.random() * (to - from) ) + from);
     }
-
-    /*
-    OnCollide(a, b)
-    {
-        if(!this.aRemover.includes(a))
-        {
-            this.assets.play("collide");
-            this.aRemover.push(a);
-        }
-        if(!this.aRemover.includes(b))
-        {
-            this.aRemover.push(b);
-        }
-
-        if(a.tag == "player" && b.tag == "enemy")
-        {
-            console.log("game over");
-            this.game.SelecScene("end");
-        }
-    }
-    */
 }
